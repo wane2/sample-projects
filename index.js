33a2123ce504698a27984e2af161b78dc1cb2c16
danny <danny@splitzcityzen@gmail.comgit config --global user.email 123456789+odin@users.noreply.github>
2023-10-24 20:43:33 +0300

    Array exercises

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-//arrays
+//arrays: special variable that can hold more than one value.
+const countries = ['kenya', 'uganda', 'mongolia', 'uzbekistan', 'turkmenistan'];
+
+countries [5] = 'usa';
+
+console.log(countries);
+
+const colours = ['black', 'white', 'purple',['yellow', 'blue', 'indigo']];
+
+console.log(colours[3][1]);
