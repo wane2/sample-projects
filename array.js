4542d3dc075e2ed794de5b80344d3be07a315f2e
danny <danny@splitzcityzen@gmail.comgit config --global user.email 123456789+odin@users.noreply.github>
2023-10-25 19:18:31 +0300

    Add array method exercises

---
+++ array.js
@@ -0,0 +1,19 @@
+//array exercise
+
+
+let fruits = ['apple', 'orange', 'banana',];
+
+fruits.unshift('cherry', 'pineapple');
+
+fruits.push('plums', 'grapes');
+
+
+console.log(fruits)
+
+fruits.splice(3, 1, 'lemon');
+
+console.log(fruits);
+
+fruits.sort();
+
+console.log(fruits);
