cf74e125c88de6fd5261f95c61e8b4d8f239429a
danny <danny@splitzcityzen@gmail.comgit config --global user.email 123456789+odin@users.noreply.github>
2023-10-30 18:54:38 +0300

    ADD project files

--- array.js
+++ array.js
@@ -1,19 +1,9 @@
-//array exercise
+//arrays
+
+let supers = ['superman', 'batman', 'flash', 'aquaman', 'thor', 'spiderman',
+              'wolverine', 'cyclops', 'proffessor x'];
 
 
-let fruits = ['apple', 'orange', 'banana',];
-
-fruits.unshift('cherry', 'pineapple');
-
-fruits.push('plums', 'grapes');
-
-
-console.log(fruits)
-
-fruits.splice(3, 1, 'lemon');
-
-console.log(fruits);
-
-fruits.sort();
-
-console.log(fruits);
+supers.forEach(
+      
+    );
---
+++ loops.js
@@ -0,0 +1,9 @@
+/*for (initializer); (condition); (finalExpression) {
+
+}*/
+
+
+for (let i = 0; i <= 30; i++) {
+    console.log(i);
+  }
+  
