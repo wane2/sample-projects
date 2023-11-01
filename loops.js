5f047c9d43d1a36017316660194f318e70593411
danny <danny@splitzcityzen@gmail.comgit config --global user.email 123456789+odin@users.noreply.github>
2023-11-01 19:38:33 +0300

    Add while and do while loop exercises

--- loops.js
+++ loops.js
@@ -1,9 +1,26 @@
-/*for (initializer); (condition); (finalExpression) {
+//while and do while loop
+let i = 0;
 
-}*/
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+const colours = ['red', 'blue', 'green', 'orange', 'purple'];
 
 
-for (let i = 0; i <= 30; i++) {
-    console.log(i);
-  }
-  
+let colour = 0;
+
+while (colour < colours.length) {
+  console.log(`${colours[colour]}`);
+  colour++;
+}
+
+
+
+let w = 0;
+
+do {
+  console.log(`the number is ${w}`);
+  w++;
+} while (i <= 10);
